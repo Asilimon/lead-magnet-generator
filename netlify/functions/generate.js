8711fd1d0f9f1443f9492725d54692dce6268742
Asilimon <asilimon@yahoo.com>
2026-05-13 12:14:00 -0400

    Update generate.js

--- netlify/functions/generate.js
+++ netlify/functions/generate.js
@@ -85,7 +85,7 @@ exports.handler = async function (event) {
       },
       body: JSON.stringify({
         model: 'claude-haiku-4-5-20251001',
-        max_tokens: 1800,
+        max_tokens: 2500,
         messages: [{ role: 'user', content: prompt }]
       })
     });
